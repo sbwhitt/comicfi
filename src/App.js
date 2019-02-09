3af1a3a19580e7c51455a3beafcd1223b9c6147f
sbwhitt <swhitt@instance-1.us-east1-b.c.named-container-229503.internal>
2019-02-09 19:18:43 +0000

    styling and dropdown filter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,88 @@
 import React, { Component } from 'react';
-import { ReactiveBase, DataSearch, ReactiveList, SingleDataList } from '@appbaseio/reactivesearch';
+import {
+  ReactiveBase,
+  DataSearch,
+  ReactiveList,
+  SingleDataList,
+  SingleDropdownList 
+} from '@appbaseio/reactivesearch';
 import './App.css';
 
 class App extends Component {
   _renderResults(res) {
     return(
-      <div style={{alignItems: "center"}}>
-        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: "1em", marginBottom: "1em"}}>
-          <a href={res.url} target="_blank">link</a>
-          <p>{res.comic}</p>
-          <p>{res.title}</p>
+      <div className="result">
+        <div className="result-content">
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <p>{res.comic}</p>
+            <p>{res.title}</p>
+          </div>
+          <a href={res.url} target="_blank"><img src={res.orig_img}/></a>
         </div>
-        <img src={res.orig_img}/>
       </div>
     );
   };
 
   render() {
     return (
-      <ReactiveBase app="xkcd,smbc,cah,qc" url="http://35.207.29.23:9200/">
-        <h1 style={{textAlign: "center", marginTop: "2em"}}>fakecomicfi</h1>
-        <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "6em"}}>
-          <div style={{width: "75%"}}>
-            <DataSearch
-              componentId="searchbox"
-              dataField={["comic", "transcript"]}
-              placeholder="Search for comics"
-              autosuggest={false}
-            />
-            <SingleDataList style={{marginLeft: "40%"}}
-              componentId="filters"
-              dataField="comic.keyword"
-              data={[
-                {label: "Cyanide and Happiness", value: "Cyanide and Happiness"},
-                {label: "Questionable Content", value: "Questionable Content"},
-                {label: "smbc", value: "smbc"},
-                {label: "xkcd", value: "xkcd"}
-              ]}
-              showSearch={false}
-            />
+      <div>
+        <h1 style={{textAlign: "center", marginTop: "1em", color: "white"}}>comicfi</h1>
+        <ReactiveBase app="xkcd,smbc,cah,qc" url="http://35.207.29.23:9200/">
+          <div className="search-wrapper">
+              <DataSearch className="search-box"
+                componentId="searchbox"
+                dataField={["comic", "transcript", "title"]}
+                placeholder="Search for comics"
+                autosuggest={false}
+                fuzziness={1}
+              />
           </div>
-          <div style={{width: "50%"}}>
-            <ReactiveList
-              componentId="list"
-              dataField="transcript.keyword"
-              react={{and: ["searchbox", "filters"]}}
-              stream={true}
-              renderData={this._renderResults}
-            />
+          <div className="results-wrapper">
+            <div className="results-list">
+              <ReactiveList
+                componentId="list"
+                dataField="transcript.keyword"
+                react={{and: ["searchbox", "comicFilters"]}}
+                stream={true}
+                renderData={this._renderResults}
+                showResultStats={false}
+              />
+            </div>
+            <div className="sidebar-container">
+              <div className="sidebar-content">
+                <SingleDropdownList className="dropdown"
+                  placeholder="Select comic..."
+                  componentId="comicFilters"
+                  dataField="comic.keyword"
+                  data={[
+                    {label: "Cyanide and Happiness", value: "Cyanide and Happiness"},
+                    {label: "Questionable Content", value: "Questionable Content"},
+                    {label: "smbc", value: "smbc"},
+                    {label: "xkcd", value: "xkcd"}
+                  ]}
+                  showSearch={false}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </ReactiveBase>
+        </ReactiveBase>
+      </div>
     );
   }
 }
 
 export default App;
+
+/*
+<SingleDataList
+                  componentId="comicFilters"
+                  dataField="comic.keyword"
+                  data={[
+                    {label: "Cyanide and Happiness", value: "Cyanide and Happiness"},
+                    {label: "Questionable Content", value: "Questionable Content"},
+                    {label: "smbc", value: "smbc"},
+                    {label: "xkcd", value: "xkcd"}
+                  ]}
+                  showSearch={false}
+                />
+*/
